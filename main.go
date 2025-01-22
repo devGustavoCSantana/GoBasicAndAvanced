853485e35cb26babda33fd74d7dea0fdf5c8b627
Gustavo Cavalcante da Fonseca Santana <gustavocavalcantegu1221@gmail.com>
2025-01-22 09:45:48 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// Função principal que é executada quando o programa é iniciado.
+func main() {
+	// A função fmt.Println imprime a string "Hello, World!" no console.
+	fmt.Println("Hello, World!")
+}
