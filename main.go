b843c6c3fcf4cd7822890b3d8fdd478087e562b3
Gustavo Cavalcante da Fonseca Santana <gustavocavalcantegu1221@gmail.com>
2025-01-22 09:53:39 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -4,19 +4,6 @@ import "fmt"
 
 // Função principal que é executada quando o programa é iniciado.
 func main() {
-	// A função fmt.Printf permite formatar a string de saída.
-	// %T exibe o tipo da variável.
-	// %v exibe o valor da variável.
-
-	// A primeira linha imprime o tipo e o valor da string "1".
-	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-
-	// A segunda linha imprime o tipo e o valor da string "1".
-	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-
-	// A terceira linha imprime o tipo e o valor da string "1".
-	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-
-	// A quarta linha imprime o tipo e o valor da string "1".
-	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	// A função fmt.Println imprime a string "Hello, World!" no console.
+	fmt.Println("Hello, World!")
 }
---
+++ variaveis/variaveisTypes.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Função principal que é executada quando o programa é iniciado.
+func main() {
+	// A função fmt.Printf permite formatar a string de saída.
+	// %T exibe o tipo da variável.
+	// %v exibe o valor da variável.
+
+	// A primeira linha imprime o tipo e o valor da string "1".
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+
+	// A segunda linha imprime o tipo e o valor da string "1".
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+
+	// A terceira linha imprime o tipo e o valor da string "1".
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+
+	// A quarta linha imprime o tipo e o valor da string "1".
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+}
